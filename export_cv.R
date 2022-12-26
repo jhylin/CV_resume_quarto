684574329d226a48c6b1c90244fc1e4fc4f682a4
Jenny HY Lin <jennyhylin@gmail.com>
2022-12-27 11:43:19 +1300

    Latest updated versions of CV/resume.

--- export_cv.R
+++ export_cv.R
@@ -1,5 +1,4 @@
 pagedown::chrome_print("index.html")
-pdftools::pdf_subset(input = "index.pdf", pages =  c(1,2,3))
+pdftools::pdf_subset(input = "index.pdf", pages = 1:3)
 fs::file_copy(path = "index_output.pdf", new_path = "index.pdf", overwrite = TRUE)
-
 
